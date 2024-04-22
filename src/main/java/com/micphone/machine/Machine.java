aea0e70a1060830b3ac0cb9b52506557719687bb
micphone <2082183611@qq.com>
2024-04-22 19:09:20 +0800

    Initial commit

---
+++ src/main/java/com/micphone/machine/Machine.java
@@ -0,0 +1,28 @@
+package com.micphone.machine;
+
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import org.slf4j.Logger;
+
+@Mod(Machine.MOD_ID)
+public class Machine {
+    public static final String MOD_ID = "machine";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Machine(IEventBus modEventBus) {
+        modEventBus.addListener(this::commonSetup);
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+}
